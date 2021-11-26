c3150a56c3e60da8d48e1a93df95a6a62bd90924
Liu Zichang <u6756985@anu.edu.au>
2021-11-26 19:07:44 +0800

    11-26

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv'
+const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn';
  const improvRNN = new mm.MusicRNN(improvCheckpoint)
 
-
-
+ //improvRNN.initialize();
+ // ​
+ // // Create a player to play the sequence we'll get from the model.
+  rnnPlayer = new mm.Player();
+ // ​rnn_steps = 20;
+  //rnn_temperature = 1.5;
+ function play() {
+   if (rnnPlayer.isPlaying()) {
+     rnnPlayer.stop();
+     return;
+   }
+}
+player = new mm.Player();
+var generateSequence;
+var predictSeq=[];
+ var notes_array = [];
 /**
  * perhaps:
  * 1.midiDrums=[,,,]
@@ -15,157 +29,88 @@ const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoi
  */
 
 
- const DRUM_CLASSES = [
-    'Kick',
-    'Snare',
-    'Hi-hat closed',
-    'Hi-hat open',
-    'Tom low',
-    'Tom mid',
-    'Tom high',
-    'Clap',
-    'Rim'];
+//  const DRUM_CLASSES = [
+//     'Kick',
+//     'Snare',
+//     'Hi-hat closed',
+//     'Hi-hat open',
+//     'Tom low',
+//     'Tom mid',
+//     'Tom high',
+//     'Clap',
+//     'Rim'];
 
-let midiDrums = [36, 38, 42, 46, 41, 43, 45, 49, 51];
-
-let reverseMidiMapping = new Map([
-    [36, 0],
-    [35, 0],
-    [38, 1],
-    [27, 1],
-    [28, 1],
-    [31, 1],
-    [32, 1],
-    [33, 1],
-    [34, 1],
-    [37, 1],
-    [39, 1],
-    [40, 1],
-    [56, 1],
-    [65, 1],
-    [66, 1],
-    [75, 1],
-    [85, 1],
-    [42, 2],
-    [44, 2],
-    [54, 2],
-    [68, 2],
-    [69, 2],
-    [70, 2],
-    [71, 2],
-    [73, 2],
-    [78, 2],
-    [80, 2],
-    [46, 3],
-    [67, 3],
-    [72, 3],
-    [74, 3],
-    [79, 3],
-    [81, 3],
-    [45, 4],
-    [29, 4],
-    [41, 4],
-    [61, 4],
-    [64, 4],
-    [84, 4],
-    [48, 5],
-    [47, 5],
-    [60, 5],
-    [63, 5],
-    [77, 5],
-    [86, 5],
-    [87, 5],
-    [50, 6],
-    [30, 6],
-    [43, 6],
-    [62, 6],
-    [76, 6],
-    [83, 6],
-    [49, 7],
-    [55, 7],
-    [57, 7],
-    [58, 7],
-    [51, 8],
-    [52, 8],
-    [53, 8],
-    [59, 8],
-    [82, 8]]);
 
   
-    function getStepVelocity(step) {
-        if (step % 4 === 0) {
-          return 'high';
-        } else if (step % 2 === 0) {
-          return 'med';
-        } else {
-          return 'low';
-        }
-      }
+//     function getStepVelocity(step) {
+//         if (step % 4 === 0) {
+//           return 'high';
+//         } else if (step % 2 === 0) {
+//           return 'med';
+//         } else {
+//           return 'low';
+//         }
+//       }
       
       
-//small draft to try the drumRNN
-const { midi, Note } = Tonal  
-const synth = new Tone.Synth().toMaster()
+// //small draft to try the drumRNN
+// const { midi, Note } = Tonal  
+// const synth = new Tone.Synth().toMaster()
 const sequence = {
-  // ticksPerQuarter: 220,
-  // totalTime: 58,
-  // timeSignatures: [
-  //   {
-  //     time: 0,
-  //     numerator: 4,
-  //     denominator: 4
-  //   }
-  // ],
-  // tempos: [
-  //   {
-  //     time: 0,
-  //     qpm: 120
-  //   }
-  // ],
+ 
   notes: [
-  //{ pitch: 74, startTime: 0, endTime: 1 }
-  { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-  { pitch: 38, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-  { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-  { pitch: 46, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-  { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3, isDrum: true },
-  { pitch: 42, quantizedStartStep: 3, quantizedEndStep: 4, isDrum: true },
-  { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-  
+    {pitch: 60, startTime: 0.0, endTime: 0.5, isDrum:true},
+	  {pitch: 60, startTime: 0.5, endTime: 1.0, isDrum:true},
+	  {pitch: 67, startTime: 1.0, endTime: 1.5, isDrum:true},
+	  {pitch: 67, startTime: 1.5, endTime: 2.0, isDrum:true},
+	  {pitch: 69, startTime: 2.0, endTime: 2.5, isDrum:true},
+	  {pitch: 69, startTime: 2.5, endTime: 3.0, isDrum:true},
+	  {pitch: 67, startTime: 3.0, endTime: 4.0, isDrum:true},
+	  {pitch: 65, startTime: 4.0, endTime: 4.5, isDrum:true},
   ],
-  quantizationInfo: {stepsPerQuarter: 4},
-  tempos: [{time: 0, qpm: 120}],
-  totalQuantizedSteps: 11
-}
+ totalTime:8
+};
 
-const quantizedSequence = mm.sequences.quantizeNoteSequence(sequence, 4);
+//const quantizedSequence = mm.sequences.quantizeNoteSequence(sequence, 4);
 
         var autoButton = new Nexus.RadioButton('#auto', {
             'size': [70, 70],
             'numberOfButtons': 1,
             'active': -1
         }) 
+ 
         
 const startProgram = async () => {
     try {
         await improvRNN.initialize()
-        let improvisedMelody = await improvRNN.continueSequence(quantizedSequence, 60, 1.1, ['Bm', 'Bbm', 'Gb7', 'F7', 'Ab', 'Ab7', 'G7', 'Gb7', 'F7', 'Bb7', 'Eb7', 'AM7'])
+       // let improvisedMelody = await improvRNN.continueSequence(quantizedSequence, 60, 1.1, ['Bm', 'Bbm', 'Gb7', 'F7', 'Ab', 'Ab7', 'G7', 'Gb7', 'F7', 'Bb7', 'Eb7', 'AM7'])
     
         const playOriginalMelody = () => {
-          sequence.notes.forEach(note => {
-            synth.triggerAttackRelease(Note.fromMidi(note.pitch), note.endTime - note.startTime, note.startTime)
-          })
+          // sequence.notes.forEach(note => {
+          // //  Note.fromMidi(note.pitch)
+          //   synth.triggerAttackRelease(Note.fromMidi(note.pitch), note.endTime - note.startTime, note.startTime)
+          // })
+          player.start(sequence);
+
         }
     
         const playGeneratedMelody = () => {
-          improvisedMelody.notes.forEach(note => {
-            synth.triggerAttackRelease(Note.fromMidi(note.pitch), note.quantizedEndStep - note.quantizedStartStep, note.quantizedStartStep)
-          })
+          // improvisedMelody.notes.forEach(note => {
+          //   synth.triggerAttackRelease(Note.fromMidi(note.pitch), note.quantizedEndStep - note.quantizedStartStep, note.quantizedStartStep)
+          // })
+          const qns = mm.sequences.quantizeNoteSequence(sequence, 4);
+ generateSequence= improvRNN
+ .continueSequence(qns, 20, 1.5);
+  //         improvRNN
+  // .continueSequence(qns, 20, 1.5)
+
+  generateSequence
+  .then((sample) => rnnPlayer.start(sample));
         }
     
      autoButton.on('change', function (v){
-         playOriginalMelody();
-        //   playGeneratedMelody();
+        // playOriginalMelody();
+           playGeneratedMelody();
         })
       } catch (error) {
         console.error(error)
@@ -173,10 +118,28 @@ const startProgram = async () => {
 }
 
 
+
+// function drum_to_note_sequence(quantize_tensor) {
+// 	var notes_array = [];
+// 	var note_index = 0;
+// 	//for (var i = 0; i < quantize_tensor.length; i++) {
+// 		var notes = quantize_tensor.notes;
+// 		if(notes.length > 0) {
+// 			for (var j = 0; j < notes.length; j++) {
+// 				notes_array[note_index] = {};
+// 				notes_array[note_index]["pitch"] = quantize_tensor.notes[j].pitch;
+// 				notes_array[note_index]["startTime"] = quantize_tensor.notes[j].startTime;
+// 				notes_array[note_index]["endTime"] = quantize_tensor.notes[j].endTime;
+//         notes_array[note_index]["isDrum"]=true;
+// 				note_index = note_index + 1;
+// 			}
+// 		}
+// //	}
+//   return notes_array;
+// }
+
+
 startProgram()
-
-
-
 
 console.clear()
         var radiobutton = new Nexus.RadioButton('#switch', {
@@ -195,12 +158,12 @@ console.clear()
             'value': 128
         })
 
-        var noteNames1 = ["C#3", "F#2", "A#2", "C2"];
+        var noteNames1 = ["Eb1", "F#2", "Bb1", "C1"];
         var keys1 = new Tone.Players({
-            "C#3": "toolkit/clap.mp3",
+            "Eb1": "toolkit/clap.mp3",
             "F#2": "toolkit/hihat-closed.mp3",
-            "A#2": "toolkit/hihat-open.mp3",
-            "C2": "toolkit/kick.mp3",
+            "Bb1": "toolkit/hihat-open.mp3",
+            "C1": "toolkit/C4.mp3",
 
         }, {
             "volume": -10,
@@ -212,13 +175,22 @@ console.clear()
         
         var loop1 = new Tone.Sequence(
             function (time, col) {
-              
+              	var note_index = 0;  
+               
                 var column = document.getElementById("seq1").currentColumn;
                 column.forEach(function (val, i) {
                     if (val) {
                         var vel = 127;
                         keys1.get(noteNames1[i]).start(time, 0, "16n", 0, vel);
                         //i: have clicked, need to store
+                      
+	
+				notes_array[note_index] = {};
+				notes_array[note_index]["pitch"] = strToMidi[noteNames1[i]];
+				notes_array[note_index]["startTime"] = i * 0.5;
+				notes_array[note_index]["endTime"] = (i+1) * 0.5;
+
+				note_index = note_index + 1;
                     }
                 });
 
@@ -229,19 +201,21 @@ console.clear()
                 }, time);
             }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n").start(0);
    
+predictSeq.push(notes_array);
+
 
 
         var keys2 = new Tone.Players({
-            "A1": "toolKit/ride.mp3",
-            "C#4": "toolKit/snare.mp3",
-            "E2": "toolKit/tom-high.mp3",
-            "F#2": "toolKit/tom-low.mp3",
+            "Eb2": "toolKit/ride.mp3",
+            "D1": "toolKit/snare.mp3",
+            "D2": "toolKit/tom-high.mp3",
+            "A1": "toolKit/tom-low.mp3",
         }, {
             "volume": -10,
             "fadeOut": "64n",
         })
         keys2.toMaster();
-        var noteNames2 = ["F#2", "E2", "C#4", "A1"];
+        var noteNames2 = ["Eb2", "D1", "D2", "A1"];
         var loop2 = new Tone.Sequence(
             function (time, col) {
                 var column = document.getElementById("seq2").currentColumn;
@@ -262,7 +236,7 @@ console.clear()
 
 
         var keys3 = new Tone.Players({
-            "F#3": "toolKit/tom-mid.mp3",
+            "C2": "toolKit/tom-mid.mp3",
             "C#3": "https://res.cloudinary.com/degnified/video/upload/v1567497318/drum_o6byub.[mp3|ogg]",
             "E3": "https://res.cloudinary.com/degnified/video/upload/v1567497318/bass_snjpzv.[mp3|ogg]",
             "A2": "https://res.cloudinary.com/degnified/video/upload/v1567497317/kick_wew9fm.[mp3|ogg]",
--- temp.js
+++ temp.js
@@ -1,36 +1,50 @@
-const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv'
+const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn';
+//const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv'
  const improvRNN = new mm.MusicRNN(improvCheckpoint)
+ improvRNN.initialize();
+// ​
+// // Create a player to play the sequence we'll get from the model.
+ rnnPlayer = new mm.Player();
+// ​rnn_steps = 20;
+ //rnn_temperature = 1.5;
+function play() {
+  if (rnnPlayer.isPlaying()) {
+    rnnPlayer.stop();
+    return;
+  }
+}
  TWINKLE_TWINKLE = {
 	notes: [
-	  {pitch: 60, startTime: 0.0, endTime: 0.5},
-	  {pitch: 60, startTime: 0.5, endTime: 1.0},
-	  {pitch: 67, startTime: 1.0, endTime: 1.5},
-	  {pitch: 67, startTime: 1.5, endTime: 2.0},
-	  {pitch: 69, startTime: 2.0, endTime: 2.5},
-	  {pitch: 69, startTime: 2.5, endTime: 3.0},
-	  {pitch: 67, startTime: 3.0, endTime: 4.0},
-	  {pitch: 65, startTime: 4.0, endTime: 4.5},
-	  {pitch: 65, startTime: 4.5, endTime: 5.0},
-	  {pitch: 64, startTime: 5.0, endTime: 5.5},
-	  {pitch: 64, startTime: 5.5, endTime: 6.0},
-	  {pitch: 62, startTime: 6.0, endTime: 6.5},
-	  {pitch: 62, startTime: 6.5, endTime: 7.0},
-	  {pitch: 60, startTime: 7.0, endTime: 8.0},  
+	  {pitch: 60, startTime: 0.0, endTime: 0.5, isDrum:true},
+	  {pitch: 60, startTime: 0.5, endTime: 1.0, isDrum:true},
+	  {pitch: 67, startTime: 1.0, endTime: 1.5, isDrum:true},
+	  {pitch: 67, startTime: 1.5, endTime: 2.0, isDrum:true},
+	  {pitch: 69, startTime: 2.0, endTime: 2.5, isDrum:true},
+	  {pitch: 69, startTime: 2.5, endTime: 3.0, isDrum:true},
+	  {pitch: 67, startTime: 3.0, endTime: 4.0, isDrum:true},
+	  {pitch: 65, startTime: 4.0, endTime: 4.5, isDrum:true},
+	  {pitch: 65, startTime: 4.5, endTime: 5.0, isDrum:true},
+	  {pitch: 64, startTime: 5.0, endTime: 5.5, isDrum:true},
+	  {pitch: 64, startTime: 5.5, endTime: 6.0, isDrum:true},
+	  {pitch: 62, startTime: 6.0, endTime: 6.5, isDrum:true},
+	  {pitch: 62, startTime: 6.5, endTime: 7.0, isDrum:true},
+	  {pitch: 60, startTime: 7.0, endTime: 8.0, isDrum:true},  
 	],
 	totalTime: 8
       };
 
       DRUMS = {
 	notes: [
+		//39,42,46,36,51,38,50,45,48
 	  { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-	//   { pitch: 38, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-	//   { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-	//   { pitch: 46, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-	//   { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3, isDrum: true },
-	//   { pitch: 42, quantizedStartStep: 3, quantizedEndStep: 4, isDrum: true },
-	//   { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-	//   { pitch: 50, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-	//   { pitch: 36, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+	   { pitch: 39, quantizedStartStep: 1, quantizedEndStep: 2, isDrum: true },
+	   { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3, isDrum: true },
+	   { pitch: 46, quantizedStartStep: 3, quantizedEndStep: 4, isDrum: true },
+	   { pitch: 51, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
+	   { pitch: 38, quantizedStartStep: 5, quantizedEndStep: 6, isDrum: true },
+	   { pitch: 45, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
+	   { pitch: 50, quantizedStartStep: 7, quantizedEndStep: 8, isDrum: true },
+	   { pitch: 48, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true }
 	//   { pitch: 38, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
 	//   { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
 	//   { pitch: 45, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
@@ -46,6 +60,8 @@ const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoi
 	totalQuantizedSteps: 11
       };
   
+
+
  player = new mm.Player();
  
       var radiobutton = new Nexus.RadioButton('#switch', {
@@ -53,6 +69,14 @@ const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoi
 	'numberOfButtons': 1,
 	'active': -1
     })
+
+
+var autoButton = new Nexus.RadioButton('#auto', {
+	'size': [70, 70],
+	'numberOfButtons': 1,
+	'active': -1
+    })
+
 
     radiobutton.on('change', function (v) {
 	if (v == 0) {
@@ -66,37 +90,83 @@ const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoi
 	}
     })
 
-    config = {
-	noteHeight: 6,
-	pixelsPerTimeStep: 30,  // like a note width
-	noteSpacing: 1,
-	noteRGB: '8, 41, 64',
-	activeNoteRGB: '240, 84, 119',
-      }
-      // Don't edit this line unless you want to break things. :)
-      visualizer = new mm.Visualizer(TWINKLE_TWINKLE, document.getElementById('canvas'), config);
+    autoButton.on('change', function (v) {
+	if (v == 0) {
+//	player.start(TWINKLE_TWINKLE);
+  const qns = mm.sequences.quantizeNoteSequence(TWINKLE_TWINKLE, 4);
+  improvRNN
+  .continueSequence(qns, 20, 1.5)
+  .then((sample) => rnnPlayer.start(sample));
+	} 
+	else if (v == -1) {  
+		player.stop();
+	//     Tone.Transport.stop()
+	//     $('#playState').text('Play')
+	}
+    })
+
+
+//     config = {
+// 	noteHeight: 6,
+// 	pixelsPerTimeStep: 30,  // like a note width
+// 	noteSpacing: 1,
+// 	noteRGB: '8, 41, 64',
+// 	activeNoteRGB: '240, 84, 119',
+//       }
+//       // Don't edit this line unless you want to break things. :)
+//       visualizer = new mm.Visualizer(TWINKLE_TWINKLE, document.getElementById('canvas'), config);
 
 
 
+
+
+//       TWINKLE_TWINKLE = {
+// 	notes: [
+// 	  {pitch: 60, startTime: 0.0, endTime: 0.5, isDrum: true},
+// 	  {pitch: 60, startTime: 0.5, endTime: 1.0, isDrum: true},
+// 	  {pitch: 67, startTime: 1.0, endTime: 1.5, isDrum: true},
+// 	  {pitch: 67, startTime: 1.5, endTime: 2.0, isDrum: true},
+// 	  {pitch: 69, startTime: 2.0, endTime: 2.5, isDrum: true},
+// 	  {pitch: 69, startTime: 2.0, endTime: 3.0, isDrum: true},
+
+// 	],
+// 	totalTime: 8
+//       };
       
-//  music_rnn = new mm.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn');
+ //music_rnn = new mm.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn');
 //  improvRNN.initialize();
 // // ​
 // // // Create a player to play the sequence we'll get from the model.
 //  rnnPlayer = new mm.Player();
 // // ​rnn_steps = 20;
 //  //rnn_temperature = 1.5;
-// // function play() {
-// //   if (rnnPlayer.isPlaying()) {
-// //     rnnPlayer.stop();
-// //     return;
-// //   }
-      
-// //   // The model expects a quantized sequence, and ours was unquantized:
-//   const qns = mm.sequences.quantizeNoteSequence(DRUMS, 4);
+// function play() {
+//   if (rnnPlayer.isPlaying()) {
+//     rnnPlayer.stop();
+//     return;
+//   }
+// }
+     
+
+
+
+//     autoButton.on('change', function (v) {
+// 	if (v == 0) {
+// 	player.start(DRUMS);
+//   const qns = mm.sequences.quantizeNoteSequence(TWINKLE_TWINKLE, 4);
 //   improvRNN
 //   .continueSequence(qns, 20, 1.5)
 //   .then((sample) => rnnPlayer.start(sample));
+// 	} else if (v == -1) {  
+// 		player.stop();
+// 	//     Tone.Transport.stop()
+// 	//     $('#playState').text('Play')
+// 	}
+//     })
+
+
+
+
 
 
    
