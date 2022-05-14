eb4d3db9dbf942390b1c03b9fe031bf2ced7d3b4
Liu Zichang <u6756985@anu.edu.au>
2022-05-14 13:57:12 +0800

    imageSize

--- final.js
+++ final.js
@@ -5,9 +5,6 @@ var bcg,bcg1;
 function preload() {
 
 	bcg=loadImage('images/bcg.jpg');
-	bcg1=loadImage('images/bcg1.jpg');
-	bcg2=loadImage('images/bcg2.jpg');
-	bcg3=loadImage('images/bcg3.jpg');
 	 bcg4=loadImage('images/bcg4.jpg');
 	game=loadImage('images/game.png');	
 	
